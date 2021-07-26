76943c380237f449157e53d089fc00ba3c0976c8
Adam Berecz <adam@laraform.io>
2021-07-26 20:12:10 +0200

    useSearch clearup

--- src/composables/useSearch.js
+++ src/composables/useSearch.js
@@ -2,45 +2,11 @@ import { ref, toRefs, computed, watch } from 'composition-api'
 
 export default function useSearch (props, context, dep)
 {
-  const { mode } = toRefs(props)
-
-  // ============ DEPENDENCIES ============
-
-  const iv = dep.iv
-
   // ================ DATA ================
 
   const search = ref(null)
 
   const input = ref(null)
-
-  // ============== COMPUTED ==============
-
-  const tagsSearchWidth = computed(() => {
-    // istanbul ignore next
-    if (search.value) {
-      return `${textWidth.value}px`
-    }
-
-    if (mode.value !== 'tags' || [null, undefined].indexOf(iv.value) !== -1 || !iv.value.length) {
-      return '100%'
-    }
-
-    return '1ch'
-  })
-
-  const textWidth = computed(() => {
-    const span = document.createElement('span')
-    span.innerHTML = search.value.replace(/ /g, '&nbsp;')
-    span.style.position = 'absolute'
-    span.style.left = '-9999px'
-    span.style.top = '-9999px'
-    document.body.append(span)
-    const width = span.offsetWidth
-    span.remove()
-
-    return width
-  })
 
 
   // =============== METHODS ==============
@@ -62,7 +28,6 @@ export default function useSearch (props, context, dep)
   return {
     search,
     input,
-    tagsSearchWidth,
     clearSearch,
     handleSearchInput,
   }
