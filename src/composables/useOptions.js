73bec416393892d23670289b2793e08c528851f1
Adam Berecz <adam@laraform.io>
2021-09-09 12:49:05 +0200

    Allow case sensitive new tags #119

--- src/composables/useOptions.js
+++ src/composables/useOptions.js
@@ -133,7 +133,7 @@ export default function useOptions (props, context, dep)
       return []
     }
 
-    return getOptionByTrackBy(search.value) !== -1 ? [] : [{
+    return getOptionByTrackBy(search.value, false) !== -1 ? [] : [{
       [valueProp.value]: search.value,
       [label.value]: search.value,
       [trackBy.value]: search.value,
@@ -381,8 +381,8 @@ export default function useOptions (props, context, dep)
   }
 
   // no export
-  const getOptionByTrackBy = (val) => {
-    return eo.value.map(o => normalize(o[trackBy.value])).indexOf(normalize(val))
+  const getOptionByTrackBy = (val, norm = true) => {
+    return eo.value.map(o => norm ? normalize(o[trackBy.value]) : o[trackBy.value]).indexOf(norm ? normalize(val) : val)
   }
 
   // no export
