4f883fd8db4938677c86e2400a220f417cde3aad
Adam Berecz <adam@laraform.io>
2022-12-21 13:52:04 +0100

    fix: norwegian chars #243

--- src/utils/normalize.js
+++ src/utils/normalize.js
@@ -1,5 +1,11 @@
 export default function normalize (str, strict = true) {
   return strict
     ? String(str).toLowerCase().trim()
-    : String(str).normalize('NFD').replace(/\p{Diacritic}/gu, '').toLowerCase().trim()
+    : String(str).toLowerCase()
+                 .normalize('NFD')
+                 .trim()
+                 .replace(new RegExp(/æ/g), 'ae')
+                 .replace(new RegExp(/œ/g), 'oe')
+                 .replace(new RegExp(/ø/g), 'o')
+                 .replace(/\p{Diacritic}/gu, '')
 }
