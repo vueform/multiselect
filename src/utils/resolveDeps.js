9b75770d26688a1fbbcddb06c5d46a7763bcaf16
Negezor <negezor@gmail.com>
2024-06-02 14:43:14 +1100

    perf(utils): composable cannot be nullish in resolveDeps

--- src/utils/resolveDeps.js
+++ src/utils/resolveDeps.js
@@ -1,13 +1,9 @@
 export default function (props, context, features, deps = {}) {
   features.forEach((composable) => {
-    /* istanbul ignore else */
-    if (composable) {
-      deps = {
-        ...deps,
-        ...composable(props, context, deps)
-      }
+    deps = {
+      ...deps,
+      ...composable(props, context, deps)
     }
-
   })
   
   return deps
