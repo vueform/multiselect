eaa714dfe76cde299f93a166f976719fc27e3fff
Negezor <negezor@gmail.com>
2024-06-02 14:59:24 +1100

    perf(utils): add early return if array lengths do not match in arraysEqual function

--- src/utils/arraysEqual.js
+++ src/utils/arraysEqual.js
@@ -1,7 +1,11 @@
 export default function arraysEqual (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  
   const array2Sorted = array2.slice().sort()
 
-  return array1.length === array2.length && array1.slice().sort().every(function(value, index) {
+  return array1.slice().sort().every(function(value, index) {
       return value === array2Sorted[index];
   })
 }
