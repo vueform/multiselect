1aa826263cc5021d7780ba4ff21b6127c52ecf3d
Negezor <negezor@gmail.com>
2024-06-02 16:56:20 +1100

    chore(utils): remove readonly from toRef

--- src/utils/toRef.js
+++ src/utils/toRef.js
@@ -1,7 +1,7 @@
-import { customRef, readonly } from 'vue'
+import { customRef } from 'vue'
 
 // Polyfill for Vue <3.3 for getters only
 // https://vuejs.org/api/reactivity-utilities.html#toref
 export default function toRef (get) {
-    return readonly(customRef(() => ({ get, set: () => { } })))
+    return customRef(() => ({ get, set: () => { } }))
 }
