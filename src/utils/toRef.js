c4051a3a341c05482e696f19c8238d7bec83d728
Adam Berecz <adam@laraform.io>
2024-06-14 01:21:12 +0200

    test: ignore toRef set

--- src/utils/toRef.js
+++ src/utils/toRef.js
@@ -3,5 +3,5 @@ import { customRef } from 'vue'
 // Polyfill for Vue <3.3 for getters only
 // https://vuejs.org/api/reactivity-utilities.html#toref
 export default function toRef (get) {
-    return customRef(() => ({ get, set: () => { } }))
+    return customRef(() => ({ get, set: /* istanbul ignore next */ () => { } }))
 }
